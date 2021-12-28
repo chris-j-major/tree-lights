703fce19407bbdac3a93ddbcbed706fc05b76db4
Chris Major <chris.major@improbable.io>
2021-12-28 17:03:37 +0000

    refactor into functions

--- generate/io.jl
+++ generate/io.jl
@@ -7,3 +7,33 @@ function load_tree( filename; has_index::Bool = true )
     end
     return map( parse_line , lines)
 end
+
+
+function generate_light_colors( frame_index , tree , f)
+    return map( p->f(frame_index,p) , tree )
+end
+
+function color_to_csv( color )
+    return "$(color[1]),$(color[2]),$(color[3])"
+end
+
+function convert_colors_to_line( frame_index , colors )
+    line = join(map( color_to_csv , colors),",")
+    return "$(frame_index-1),$(line)"
+end
+
+function header(light_count)
+    lights = join([ "R_$i,G_$i,B_$i" for i = 0:(light_count-1)],",")
+    return "FRAME_ID,$(lights)"
+end
+
+function export_field_function(filename,f,tree,frames)
+    open("input/patterns/$filename.csv","w") do io
+        println(io,header(length(tree)))
+        for frame_index = 1:frames
+            colors = generate_light_colors( frame_index , tree , f )
+            output_line = convert_colors_to_line( frame_index , colors )
+            println(io,output_line)
+        end
+    end
+end
--- generate/run.jl
+++ generate/run.jl
@@ -1,34 +1,9 @@
 include("io.jl")
 
-tree = load_tree("input/trees/pcam_coords.csv")
-
 function field_function( frame_index , position )
     return (255,0,0)
 end
 
-function generate_light_colors( frame_index )
-    return map( p->field_function(frame_index,p) , tree )
-end
+tree = load_tree("input/trees/pcam_coords.csv")
 
-function color_to_csv( color )
-    return "$(color[1]),$(color[2]),$(color[3])"
-end
-
-function convert_colors_to_line( frame_index , colors )
-    line = join(map( color_to_csv , colors),",")
-    return "$(frame_index-1),$(line)"
-end
-
-function header(light_count)
-    lights = join([ "R_$i,G_$i,B_$i" for i = 0:(light_count-1)],",")
-    return "FRAME_ID,$(lights)"
-end
-
-open("input/patterns/field.csv","w") do io
-    println(io,header(length(tree)))
-    for frame_index = 1:100
-        colors = generate_light_colors( frame_index )
-        output_line = convert_colors_to_line( frame_index , colors )
-        println(io,output_line)
-    end
-end
+export_field_function("field",field_function,tree,100)
