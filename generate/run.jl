0bc66c8d5d7139277e2c374fa0c962f78f86a8bc
Chris Major <chris.major@improbable.io>
2021-12-28 14:47:43 +0000

    initial field generator version

--- generate/run.jl
+++ generate/run.jl
@@ -1,3 +1,13 @@
 include("io.jl")
 
 tree = load_tree("input/trees/pcam_coords.csv")
+
+function field_function( frame_index , position )
+    return (255,0,0)
+end
+
+for frame_index = 1:100
+    println("Processing $frame_index")
+    colors = map( p->field_function(frame_index,p) , tree )
+    println(colors)
+end
