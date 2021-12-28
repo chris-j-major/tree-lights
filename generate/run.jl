fc268284d1857367f957d3a96aa297fcdb342272
Chris Major <chris.major@improbable.io>
2021-12-28 17:07:41 +0000

    Working field colour function

--- generate/run.jl
+++ generate/run.jl
@@ -1,7 +1,10 @@
 include("io.jl")
 
 function field_function( frame_index , position )
-    return (255,0,0)
+    r = (sin(frame_index*0.1+position[1]*4.0)+1)*128
+    g = (sin(frame_index*0.1+position[2]*4.0)+1)*128
+    b = (sin(frame_index*0.1+position[3]*4.0)+1)*128
+    return ( convert(UInt8, floor(r)), convert(UInt8, floor(g)) ,convert(UInt8, floor(b)))
 end
 
 tree = load_tree("input/trees/pcam_coords.csv")
