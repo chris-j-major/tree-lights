ede7f6e498e81a9670862583dd273ba2003a42b2
Chris Major <chris.major@improbable.io>
2021-12-28 17:01:00 +0000

    working export function

--- generate/run.jl
+++ generate/run.jl
@@ -6,8 +6,29 @@ function field_function( frame_index , position )
     return (255,0,0)
 end
 
-for frame_index = 1:100
-    println("Processing $frame_index")
-    colors = map( p->field_function(frame_index,p) , tree )
-    println(colors)
+function generate_light_colors( frame_index )
+    return map( p->field_function(frame_index,p) , tree )
+end
+
+function color_to_csv( color )
+    return "$(color[1]),$(color[2]),$(color[3])"
+end
+
+function convert_colors_to_line( frame_index , colors )
+    line = join(map( color_to_csv , colors),",")
+    return "$(frame_index-1),$(line)"
+end
+
+function header(light_count)
+    lights = join([ "R_$i,G_$i,B_$i" for i = 0:(light_count-1)],",")
+    return "FRAME_ID,$(lights)"
+end
+
+open("input/patterns/field.csv","w") do io
+    println(io,header(length(tree)))
+    for frame_index = 1:100
+        colors = generate_light_colors( frame_index )
+        output_line = convert_colors_to_line( frame_index , colors )
+        println(io,output_line)
+    end
 end
