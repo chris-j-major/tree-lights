2b3aa314c79bdccf04dfad37228940ac2d892c3b
Chris Major <chris.major@improbable.io>
2022-01-01 18:09:14 +0000

    repositioned global warming tree

--- generate/run.jl
+++ generate/run.jl
@@ -38,10 +38,73 @@ function twist_smooth_function( frame_index , position )
     return linear_rainbow(theta)
 end
 
+loaded_images = Dict()
+using Images, FileIO
+
+function load_year( year )
+    # specify the path to your local image file
+    img_path = "./data-download/$year.jpg"
+    return load(img_path)
+end
+
+function global_temp( frame_index , position )
+    if !haskey(loaded_images,frame_index)
+        loaded_images[frame_index] = load_year( frame_index + 1885)
+    end
+    image = loaded_images[frame_index]
+    (height,width) = size(image)
+    (x,y) = project(position,width,height)
+    c = image[y,x]
+    return (red(c)*255, green(c)*255, blue(c)*255)
+end
+
+to_degrees(x) = 180.0*x/π
+
+function clamp(x,min,max)
+    if x > max
+        return max
+    end
+    if x < min
+        return min
+    end
+    return x
+end
+
+function project( position , width , height )
+    long = atan( position[1],position[2])   # -2π -> 0 -> 2π
+    lat = atan( (position[3]-1.0), norm(position[1],position[2]) )  # -π -> 0 -> π 
+
+    lat_adjusted = clamp(lat * 2.0,-2π,2π)
+    curve_correction = clamp( 1.0 - abs(lat_adjusted*0.2), 0.5 , 1.0)
+    long_adjusted = clamp( long*curve_correction ,-π,π)
+
+    xo = (long_adjusted / 2π + 0.5)
+    yo = -lat_adjusted / 2π   + 0.5
+
+    x = 1+convert(Int,round((width*xo+width)%(width-1)))
+    y = 1+convert(Int,round((height*yo+height)%(height-1)))
+
+    return (x,y)
+end
+
+function build_light_map_image( positions , frame_index)
+    if !haskey(loaded_images,frame_index)
+        loaded_images[frame_index] = load_year( 20 + 1885)
+    end
+    image = loaded_images[frame_index]
+    (height,width) = size(image)
+    for position in positions
+        (x,y) = project(position,width,height)
+        image[y,x] = RGB24(0)
+    end
+    save("out.png",image)
+end
+
 fields = Dict(
     "sins" => (field_function,100),
     "twist" => (twist_sharp_function,100),
     "twist-smooth" => (twist_smooth_function,100),
+    "global-temp" => (global_temp,2020-1885)
 )
 
 simulations = Dict(
@@ -86,3 +149,5 @@ for (tree_name,tree_spec) in trees
 end
 
 write_tree_details(tree_details);
+
+build_light_map_image( load_tree("input/trees/coords_2021.csv"; has_index=false) , 50)
